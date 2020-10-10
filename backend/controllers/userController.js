11b90ba9dcfb92e2d9bfaefb926874564b721f0c
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2020-10-10 11:33:32 +0300

    front end login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,6 @@ import User from '../models/userModel.js';
 // @route     POST /api/users/login
 // @access    Public
 const authUser = asyncHandler(async (req, res) => {
-  console.log(req.body);
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
