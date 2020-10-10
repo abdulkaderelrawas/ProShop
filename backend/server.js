e42147589c10292d3163c8a642fdf2597c04a179
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2020-10-10 10:32:59 +0300

    backend registration & backend user Auth using JWT

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -13,11 +14,14 @@ const PORT = process.env.PORT || 5000;
 
 const app = new express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running ...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
