25c6847bc3b560b90e87221833bb7791a1a6aa7a
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2020-10-17 19:47:39 +0300

    Preparation for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running ...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -38,8 +34,19 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 const __dirname = path.resolve();
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running ...');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
