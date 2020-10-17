09ac85235d8008b75ff0a80bb69922c9541e564e
Abdulkader El Rawas <63848905+abdulkaderelrawas@users.noreply.github.com>
2020-10-17 18:52:05 +0300

    top products carousel

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -20,6 +20,9 @@ import {
   PRODUCT_CREATE_REVIEW_REQUEST,
   PRODUCT_CREATE_REVIEW_RESET,
   PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_TOP_FAIL,
+  PRODUCT_TOP_REQUEST,
+  PRODUCT_TOP_SUCCESS,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -129,6 +132,22 @@ export const productReviewCreateReducer = (state = {}, action) => {
 
     case PRODUCT_CREATE_REVIEW_RESET:
       return {};
+
+    default:
+      return state;
+  }
+};
+
+export const productTopRatedReducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case PRODUCT_TOP_REQUEST:
+      return { loading: true, products: [] };
+
+    case PRODUCT_TOP_SUCCESS:
+      return { loading: false, products: action.payload };
+
+    case PRODUCT_TOP_FAIL:
+      return { loading: false, error: action.payload };
 
     default:
       return state;
